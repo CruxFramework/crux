4bb56b2e73fa3fa13c55e6586caec3aef05b86f5
gessedafe <gessedafe>
2009-11-16 21:42:53 +0000

    BUGFIX - DynaTabs - The class SimpleDecoratedPanel caused compilation error when written in the same file of the DynaTabs class. Moved it to its own file.

--- CruxAdvancedWidgets/src/br/com/sysmap/crux/advanced/client/dynatabs/DynaTabs.java
+++ CruxAdvancedWidgets/src/br/com/sysmap/crux/advanced/client/dynatabs/DynaTabs.java
@@ -26,12 +26,8 @@ import br.com.sysmap.crux.core.client.screen.ModuleComunicationException;
 
 import com.google.gwt.event.logical.shared.BeforeSelectionEvent;
 import com.google.gwt.event.logical.shared.BeforeSelectionHandler;
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.Element;
-import com.google.gwt.user.client.ui.CellPanel;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.TabPanel;
-import com.google.gwt.user.client.ui.Widget;
 
 /**
  * TODO - Gessé - Comment
@@ -270,78 +266,5 @@ public class DynaTabs extends Composite
 				}
 			}			
 		};
-	}
-}
-
-/**
- * TODO - Gessé - Comment this
- * @author Gessé S. F. Dafé - <code>gessedafe@gmail.com</code>
- */
-class SimpleDecoratedPanel extends CellPanel
-{
-	private Element line;
-	private Element leftCell;
-	private Element centerCell;
-	private Element rightCell;
-	
-	public SimpleDecoratedPanel()
-	{
-		getTable().setClassName("");
-	
-		line = DOM.createTR();
-		leftCell = createTd("flapLeft", true);
-		centerCell = createTd("flapCenter", false);
-		centerCell.setPropertyInt("colSpan", 2);
-		rightCell = createTd("flapRight", false);
-		
-	    DOM.appendChild(line, leftCell);
-	    DOM.appendChild(line, centerCell);
-	    DOM.appendChild(line, rightCell);
-	    DOM.appendChild(getBody(), line);
-	    	    
-	    setSpacing(0);
-	}
-	
-	/**
-	 * Adds a widget to the body of the panel (middle center cell)
-	 * @param w
-	 */
-	public void setContentWidget(Widget w)
-	{
-		cleanEmptySpaces(centerCell);
-		add(w, centerCell);
-	}
-	
-	/**
-	 * @param middleCenterCell2
-	 */
-	private void cleanEmptySpaces(Element cell)
-	{
-		String text = cell.getInnerText();
-		
-		if(text != null && text.trim().length() == 0)
-		{
-			cell.setInnerText("");
-		}
-	}
-
-	/**
-	 * Creates a TD with the given style name 
-	 * @param styleName
-	 * @return
-	 */
-	private Element createTd(String styleName, boolean fillWithBlank)
-	{
-		Element td = DOM.createTD();
-		td.setClassName(styleName);
-		
-		if(fillWithBlank)
-		{
-			td.setInnerHTML("&nbsp;");
-		}
-		
-		td.setPropertyString("align", "center");
-		td.setPropertyString("valign", "middle");		
-		return td;
 	}
 }
