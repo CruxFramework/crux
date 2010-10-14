e91e2d0fcbfd175fd574417869fd1504211fe271
thiago@cruxframework.org <thiago@cruxframework.org>
2010-10-14 17:48:09 +0000

    BUGFIX - Issue #193

--- CruxGWTWidgets/src/br/com/sysmap/crux/gwt/client/LazyPanelFactory.java
+++ CruxGWTWidgets/src/br/com/sysmap/crux/gwt/client/LazyPanelFactory.java
@@ -16,7 +16,10 @@
 package br.com.sysmap.crux.gwt.client;
 
 import br.com.sysmap.crux.core.client.declarative.DeclarativeFactory;
-import br.com.sysmap.crux.core.client.screen.LayFactory;
+import br.com.sysmap.crux.core.client.screen.HasWidgetsFactory;
+import br.com.sysmap.crux.core.client.screen.HasWidgetsHandler;
+import br.com.sysmap.crux.core.client.screen.InterfaceConfigException;
+import br.com.sysmap.crux.core.client.screen.LazyFactory;
 
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
@@ -25,35 +28,68 @@ import com.google.gwt.user.client.ui.LazyPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
- * A LayzyPanel that encapsulate a SimplePanelFactory
- * @author Thiago Bustamante
+ * A Panel which content is only rendered when it becomes visible for the first time.
+ * 
+ * @author Thiago da Rosa de Bustamante
  */
-@DeclarativeFactory(id="lazyPanel", library="gwt")
-public class LazyPanelFactory extends PanelFactory<LazyPanel> implements LayFactory<LazyPanel>
+@DeclarativeFactory(id="lazyPanel", library="gwt", lazy=true)
+public class LazyPanelFactory extends PanelFactory<LazyPanel> implements LazyFactory<LazyPanel>, HasWidgetsFactory<LazyPanel>
 {
 	protected com.google.gwt.user.client.ui.LazyPanel lazyPanelWidget;
 	
+	/**
+	 * @see br.com.sysmap.crux.core.client.screen.WidgetFactory#instantiateWidget(com.google.gwt.dom.client.Element, java.lang.String)
+	 */
 	@Override
 	public LazyPanel instantiateWidget(final Element element, String widgetId) 
 	{
-		LazyPanel result =  new LazyPanel()
-		{
+		class LazyPanelImpl extends LazyPanel implements br.com.sysmap.crux.core.client.screen.LazyPanel{
 			private String innerHTML = element.getInnerHTML();
+			
 			@Override
 			protected Widget createWidget() 
+			{
+				if (isScreenParsing())
+				{
+					createWidgetAsync();
+					return null;
+				}
+				else
+				{
+					doCreateWidget();
+					return getWidget();
+				}
+			}
+
+			private void createWidgetAsync()
 			{
 				Scheduler.get().scheduleDeferred(new ScheduledCommand()
 				{
 					public void execute()
 					{
-						getElement().setInnerHTML(innerHTML);
-						parseDocument();
+						doCreateWidget();
 					}
 				});
-				return null;
+			}			
+
+			private void doCreateWidget()
+			{
+				getElement().setInnerHTML(innerHTML);
+				innerHTML = null;
+				parseDocument();
 			}
-		};
+		}
+		LazyPanel result =  new LazyPanelImpl();
 		element.setInnerHTML("");
+		HasWidgetsHandler.handleWidgetElement(result, widgetId, "gwt_lazyPanel");
 		return result;
+	}
+
+	/**
+	 * @see br.com.sysmap.crux.core.client.screen.HasWidgetsFactory#add(com.google.gwt.user.client.ui.Widget, com.google.gwt.user.client.ui.Widget, com.google.gwt.dom.client.Element, com.google.gwt.dom.client.Element)
+	 */
+	public void add(LazyPanel parent, Widget child, Element parentElement, Element childElement) throws InterfaceConfigException
+	{
+		parent.add(child);
 	}
 }
