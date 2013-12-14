d782ff3ea589c1546af5861ea8acec9609c26ab8
gesse@cruxframework.org <gesse@cruxframework.org>
2013-12-14 21:31:37 +0000

    IGNORE - DIV based progress box, with pure CSS3 animation

--- Crux/src/ui/widgets/org/cruxframework/crux/widgets/client/dialog/ProgressBox.java
+++ Crux/src/ui/widgets/org/cruxframework/crux/widgets/client/dialog/ProgressBox.java
@@ -1,5 +1,9 @@
 package org.cruxframework.crux.widgets.client.dialog;
 
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+
 /**
  * A simple dialog which shows a progress message
  * @author Gesse Dafe
@@ -32,8 +36,29 @@ public class ProgressBox extends AbstractDialogBox
 	{
 		ProgressBox progressDialog = new ProgressBox(); 
 		progressDialog.setMessage(message);
+		progressDialog.setWidget(createInfiniteProgressBar());
 		progressDialog.show();
 		progressDialog.center();
 		return progressDialog;
+	}
+
+	/**
+	 * Creates a progress bar animation to be inserted in progress box
+	 * @return
+	 */
+	private static Widget createInfiniteProgressBar() 
+	{
+		FlowPanel bar = new FlowPanel();
+		bar.setStyleName("crux-InfinityProgressBar");
+		
+		SimplePanel slot = new SimplePanel();
+		slot.setStyleName("progressBarSlot");
+		bar.add(slot);
+		
+		SimplePanel fill = new SimplePanel();
+		fill.setStyleName("progressBarFill");
+		bar.add(fill);
+
+		return bar;
 	}	
 }
