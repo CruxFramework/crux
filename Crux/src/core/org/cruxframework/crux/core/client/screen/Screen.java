c1d785b4738c0a408b3ff97587b079ae9e84a3ba
samuel@cruxframework.org <samuel@cruxframework.org>
2014-01-23 14:05:24 +0000

    BUGFIX removing duplicated method.

--- Crux/src/core/org/cruxframework/crux/core/client/screen/Screen.java
+++ Crux/src/core/org/cruxframework/crux/core/client/screen/Screen.java
@@ -664,16 +664,7 @@ public class Screen
 		String userAgent = Window.Navigator.getUserAgent().toLowerCase();
 		return (userAgent.indexOf("iphone") > 0 || userAgent.indexOf("ipod") > 0 || userAgent.indexOf("ipad") > 0);
 	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public static boolean isTouchDevice()
-	{
-		return isIos() || isAndroid();
-	}
-	
+
 	/**
 	 * 
 	 * @return
