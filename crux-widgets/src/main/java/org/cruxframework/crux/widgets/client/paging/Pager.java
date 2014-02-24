83aadf72fb0d7cbff185b66a4d3e20c59562e950
samuel@cruxframework.org <samuel@cruxframework.org>
2014-02-24 11:54:34 +0000

    BUGFIX - Issue #319

--- crux-widgets/src/main/java/org/cruxframework/crux/widgets/client/paging/Pager.java
+++ crux-widgets/src/main/java/org/cruxframework/crux/widgets/client/paging/Pager.java
@@ -2,8 +2,12 @@ package org.cruxframework.crux.widgets.client.paging;
 
 import org.cruxframework.crux.widgets.client.event.paging.HasPageHandlers;
 
+import com.google.gwt.user.client.ui.HasEnabled;
+import com.google.gwt.user.client.ui.HasVisibility;
+import com.google.gwt.user.client.ui.IsWidget;
 
-public interface Pager extends HasPageHandlers
+
+public interface Pager extends IsWidget, HasPageHandlers, HasVisibility, HasEnabled
 {
 	void update(int currentPage, boolean isLastPage);
 	void setPageable(Pageable pageable);
