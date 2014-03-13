cc91d0c46c2a3a990a2540ea28d789058a592b59
samuel@cruxframework.org <samuel@cruxframework.org>
2014-03-13 21:38:13 +0000

    ENHANCEMENT adding client deferred binding error log

--- crux-dev/src/main/java/org/cruxframework/crux/core/client/errors/ErrorHandlerImpl.java
+++ crux-dev/src/main/java/org/cruxframework/crux/core/client/errors/ErrorHandlerImpl.java
@@ -15,21 +15,15 @@
  */
 package org.cruxframework.crux.core.client.errors;
 
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.cruxframework.crux.core.client.Crux;
-import org.cruxframework.crux.core.client.remote.ErrorHandlerService;
-import org.cruxframework.crux.core.client.remote.ErrorHandlerServiceAsync;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Document;
 import com.google.gwt.logging.client.LogConfiguration;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
@@ -39,52 +33,7 @@ import com.google.gwt.user.client.ui.Widget;
 public class ErrorHandlerImpl implements ErrorHandler, ValidationErrorHandler
 {
 	private static Logger logger = Logger.getLogger(Crux.class.getName());
-	
-	/**
-	 * Test Deferred Binding Client LOG feature
-	 */
-	//TODO: improve this calling Crux.properties
-	private static boolean dialogLogErrorEnabled = false;
-	//Call the service that will read the system error msg
-	private static ErrorHandlerServiceAsync errorHandlerService = GWT.create(ErrorHandlerService.class);
-	
-	//In order to make it work, insert this in a client pom project:
-	/*
-	<!-- Run GWT instrumentation -->
-    <plugin>
-        <groupId>org.codehaus.mojo</groupId>
-        <artifactId>aspectj-maven-plugin</artifactId>
-        <version>1.5</version>
-        <configuration>
-            <weaveDependencies>
-                <weaveDependency>
-                     <groupId>org.cruxframework</groupId>
-                     <artifactId>crux-dev</artifactId>
-                </weaveDependency>
-                <weaveDependency>
-                    <groupId>com.google.gwt</groupId>
-                    <artifactId>gwt-dev</artifactId>
-                </weaveDependency>
-            </weaveDependencies>
-            <complianceLevel>1.6</complianceLevel>
-            <source>1.6</source>
-            <target>1.6</target>
-            <Xlint>ignore</Xlint>
-        </configuration>
-        <executions>
-            <execution>
-                <phase>process-sources</phase>
-                <goals>
-                    <goal>compile</goal>
-                </goals>
-            </execution>
-        </executions>
-    </plugin>
-	*/
-	/**
-	 * END: Test Deferred Binding Client LOG feature
-	 */
-	
+
 	/**
 	 * 
 	 */
@@ -108,34 +57,15 @@ public class ErrorHandlerImpl implements ErrorHandler, ValidationErrorHandler
 	{
 		if (t != null)
 		{
-			//TODO: create a new errorHandler and a new entry point. This is here today only for tests! 
-			if(dialogLogErrorEnabled)
+			if (LogConfiguration.loggingIsEnabled())
 			{
-				errorHandlerService.getError(new AsyncCallback<ArrayList<Throwable>>() 
-				{
-					@Override
-					public void onSuccess(ArrayList<Throwable> result) 
-					{
-						Label label = new Label();
-						label.setText(result.toString());
-						Document.get().getBody().appendChild(label.getElement());	
-					}
-					
-					@Override
-					public void onFailure(Throwable exception) 
-					{
-						Window.alert(exception.getMessage());
-					}
-				});
-			} else
-			{
-				if (LogConfiguration.loggingIsEnabled())
-				{
-					logger.log(Level.SEVERE, errorMessage==null?"":errorMessage, t);
-				}
-				GWT.log(errorMessage, t);
-				Window.alert(errorMessage);
+				logger.log(Level.SEVERE, errorMessage==null?"":errorMessage, t);
 			}
+			GWT.log(errorMessage, t);
+		}
+		if (errorMessage != null)
+		{
+			Window.alert(errorMessage);
 		}
 	}
 
