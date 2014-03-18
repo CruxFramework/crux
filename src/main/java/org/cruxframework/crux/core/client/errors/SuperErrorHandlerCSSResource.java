bc135181f179d9c1e56c546311407bb17e521420
samuel@cruxframework.org <samuel@cruxframework.org>
2014-03-18 00:25:03 +0000

    ENHANCEMENT adding client deferred binding error log

---
+++ src/main/java/org/cruxframework/crux/core/client/errors/SuperErrorHandlerCSSResource.java
@@ -0,0 +1,12 @@
+package org.cruxframework.crux.core.client.errors;
+
+import com.google.gwt.resources.client.CssResource;
+
+public interface SuperErrorHandlerCSSResource extends CssResource
+{
+	String errorMsgContainer();
+	String errorMsgContainerHeader();
+	String errorMsgContainerBody();
+	String fadeInUp();
+	String animated();
+}
---
+++ src/main/java/org/cruxframework/crux/core/client/errors/SuperErrorHandlerResource.java
@@ -0,0 +1,9 @@
+package org.cruxframework.crux.core.client.errors;
+
+import com.google.gwt.resources.client.ClientBundle;
+
+public interface SuperErrorHandlerResource extends ClientBundle
+{
+	@Source("supererrorhandler.css")
+	SuperErrorHandlerCSSResource css();
+}
