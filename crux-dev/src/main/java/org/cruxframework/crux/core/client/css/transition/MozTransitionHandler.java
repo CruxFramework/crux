8859286c99033220db79fbe0b06b6426729fc8c1
thiago@cruxframework.org <thiago@cruxframework.org>
2014-03-19 11:56:39 +0000

    new animation helper class

---
+++ crux-dev/src/main/java/org/cruxframework/crux/core/client/css/transition/MozTransitionHandler.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2011 cruxframework.org.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.cruxframework.crux.core.client.css.transition;
+
+import com.google.gwt.user.client.Element;
+
+/**
+ * @author Thiago da Rosa de Bustamante
+ *
+ */
+class MozTransitionHandler extends BaseTransitionHandler
+{
+	protected native void fadeOut(Element el, double duration)/*-{
+		el.style.MozTransitionProperty = 'opacity';
+		el.style.MozTransitionDelay = '0';
+		if (duration == 0)
+		{
+			el.style.MozTransitionDuration = '';
+			el.style.MozTransitionTimingFunction = '';
+		}
+		else
+		{
+			el.style.MozTransitionDuration = duration+'ms';
+			el.style.MozTransitionTimingFunction = 'ease-out';
+		}
+	
+		el.style.opacity = 0;
+	}-*/;
+
+	protected native void fadeIn(Element el, double duration, double delay)/*-{
+		el.style.MozTransitionProperty = 'opacity';
+		if (duration == 0)
+		{
+			el.style.MozTransitionDelay = '0';
+			el.style.MozTransitionDuration = '';
+			el.style.MozTransitionTimingFunction = '';
+		}
+		else
+		{
+			el.style.MozTransitionDelay = ''+delay;
+			el.style.MozTransitionDuration = duration+'ms';
+			el.style.MozTransitionTimingFunction = 'ease-out';
+		}
+	
+		el.style.opacity = 1;
+	}-*/;
+
+	protected native void setHeight(Element el, String height, int duration)/*-{
+		el.style.MozTransitionProperty = 'height';
+		el.style.MozTransitionDelay = '0';
+		if (duration == 0)
+		{
+			el.style.MozTransitionDuration = '';
+			el.style.MozTransitionTimingFunction = '';
+		}
+		else
+		{
+			el.style.MozTransitionDuration = duration+'ms';
+			el.style.MozTransitionTimingFunction = 'ease-out';
+		}
+
+		el.style.height = height;
+	}-*/;
+
+	protected native void clearTransitionProperties(Element el)/*-{
+		el.style.MozTransitionProperty = 'all';
+		el.style.MozTransitionDuration = '';
+		el.style.MozTransitionTimingFunction = '';
+	}-*/;
+
+	protected native void translateX(Element el, int diff)/*-{
+		el.style.MozTransitionProperty = 'all';
+		el.style.MozTransitionDuration = '';
+		el.style.MozTransitionTimingFunction = '';
+		el.style.MozTransitionDelay = '0';
+		el.style.MozTransform = 'translate(' + diff + 'px,0px)';
+	}-*/;
+
+	protected native void translateX(Element el, int diff, int duration)/*-{
+		el.style.MozTransitionProperty = 'all';
+		el.style.MozTransitionDelay = '0';
+		if (duration == 0)
+		{
+			el.style.MozTransitionDuration = '';
+			el.style.MozTransitionTimingFunction = '';
+		}
+		else
+		{
+			el.style.MozTransitionDuration = duration+'ms';
+			el.style.MozTransitionTimingFunction = 'ease-out';
+		}
+
+		el.style.MozTransform = 'translate(' + diff + 'px,0px)';
+	}-*/;
+
+	protected native void resetTransition(Element el)/*-{
+		el.style.MozTransform = 'translate(0px,0px)';
+	}-*/;
+}
---
+++ crux-dev/src/main/java/org/cruxframework/crux/core/client/css/transition/Transition.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2011 cruxframework.org.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.cruxframework.crux.core.client.css.transition;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * @author Thiago da Rosa de Bustamante
+ *
+ */
+public class Transition
+{
+	private static TransitionHandler transitionHandler = null;
+
+	/**
+	 * 
+	 * @param widget
+	 * @param diff
+	 * @param callback
+	 */
+	public static void translateX(Widget widget, int diff, Callback callback)
+	{
+		getTransitionHandler().translateX(widget, diff, callback);
+	}
+	
+	/**
+	 * 
+	 * @param widget
+	 * @param diff
+	 * @param duration
+	 * @param callback
+	 */
+	public static void translateX(Widget widget, int diff, int duration, Callback callback)
+	{
+		getTransitionHandler().translateX(widget, diff, duration, callback);
+	}
+	
+	/**
+	 * 
+	 * @param widget
+	 * @param height
+	 * @param duration
+	 * @param callback
+	 */
+	public static void setHeight(Widget widget, String height, int duration, Callback callback)
+	{
+		getTransitionHandler().setHeight(widget, height, duration, callback);
+	}
+
+	/**
+	 * 
+	 * @param widget
+	 * @param height
+	 * @param duration
+	 * @param callback
+	 */
+	public static void setHeight(Widget widget, int height, int duration, Callback callback)
+	{
+		getTransitionHandler().setHeight(widget, height, duration, callback);
+	}
+
+	/**
+	 * 
+	 * @param widget
+	 */
+	public static void resetTransition(Widget widget)
+	{
+		getTransitionHandler().resetTransition(widget);
+	}
+
+	/**
+	 * 
+	 * @param widget
+	 */
+	public static void hideBackface(Widget widget)
+	{
+		getTransitionHandler().hideBackface(widget);
+	}
+	
+	/**
+	 * 
+	 * @param outWidget
+	 * @param inWidget
+	 * @param duration
+	 * @param callback
+	 */
+	public static void fade(final Widget outWidget, final Widget inWidget, final int duration, final Callback callback)
+	{
+		inWidget.getElement().getStyle().setOpacity(0);
+		Scheduler.get().scheduleDeferred(new ScheduledCommand()
+		{
+			@Override
+			public void execute()
+			{
+				getTransitionHandler().fade(outWidget, inWidget, duration, callback);
+			}
+		});
+	}
+	
+	/**
+	 * 
+	 * @param outWidget
+	 * @param duration
+	 * @param callback
+	 */
+	public static void fadeOut(Widget outWidget, int duration, Callback callback)
+	{
+		getTransitionHandler().fadeOut(outWidget, duration, callback);
+	}
+	
+	/**
+	 * 
+	 * @param widget
+	 */
+	public static void clearFadeTransitions(Widget widget)
+    {
+		getTransitionHandler().clearFadeTransitions(widget);
+    }
+	
+	/**
+	 * 
+	 * @param inWidget
+	 * @param duration
+	 * @param callback
+	 */
+	public static void fadeIn(final Widget inWidget, final int duration, final Callback callback)
+	{
+		inWidget.getElement().getStyle().setOpacity(0);
+		Scheduler.get().scheduleDeferred(new ScheduledCommand()
+		{
+			@Override
+			public void execute()
+			{
+				getTransitionHandler().fadeIn(inWidget, duration, callback);
+			}
+		});
+	}
+
+	
+	/**
+	 * 
+	 * @author Thiago da Rosa de Bustamante
+	 *
+	 */
+	public static interface Callback
+	{
+		void onTransitionCompleted();
+	}
+
+	private static TransitionHandler getTransitionHandler()
+	{
+		if (transitionHandler == null)
+		{
+			transitionHandler = GWT.create(TransitionHandler.class);
+		}
+		return transitionHandler;
+	}
+	
+	static interface TransitionHandler
+	{
+		void translateX(Widget widget, int diff, Callback callback);
+		void translateX(Widget widget, int diff, int duration, Callback callback);
+		void setHeight(Widget widget, int height, int duration, Callback callback);
+		void setHeight(Widget widget, String height, int duration, Callback callback);
+		void resetTransition(Widget widget);
+		void hideBackface(Widget widget);
+		void fade(Widget outWidget, Widget inWidget, int duration, Callback callback);
+		void fadeOut(Widget outWidget, int duration, Callback callback);
+		void fadeIn(Widget InWidget, int duration, Callback callback);
+		void clearFadeTransitions(Widget widget);
+	}
+}
