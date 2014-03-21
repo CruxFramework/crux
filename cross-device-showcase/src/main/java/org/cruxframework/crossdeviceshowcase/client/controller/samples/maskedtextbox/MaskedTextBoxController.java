83b70a1352861f59bef80de55cfc91bbabd1731c
claudio.junior@cruxframework.org <claudio.junior@cruxframework.org>
2014-03-21 17:19:31 +0000

    changes to new showcase

--- cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/maskedtextbox/MaskedTextBoxController.java
+++ cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/maskedtextbox/MaskedTextBoxController.java
@@ -14,19 +14,64 @@ import com.google.gwt.user.client.ui.Widget;
 @Controller("maskedTextBoxController")
 public class MaskedTextBoxController 
 {
+	
+	private MaskedTextBox getDateInput()
+	{
+		return View.of(this).getWidget("dateInput", MaskedTextBox.class);
+	}
+	
+	private Widget getButton()
+	{
+		return View.of(this).getWidget("btnParseDate");
+	}
+	
 	@Expose
 	public void readDate()
 	{
-		MaskedTextBox widget = View.of(this).getWidget("dateInput", MaskedTextBox.class);
-		Widget button = View.of(this).getWidget("btnParseDate");
+		MaskedTextBox widget = this.getDateInput();
 		
-		Date date = (Date) widget.getUnformattedValue();
-		if(date != null){
-			FlatMessageBox.show("The date you've informed: " + date, MessageType.INFO);
-		}else{
-			FlatMessageBox.show("Did you forgot to fill the input?", MessageType.WARN);
-			widget.setStyleName("warn", true);
-			button.setStyleName("warn", true);
+        if(widget.getValue().isEmpty())
+        {
+        	this.changeState("warn");
+        	FlatMessageBox.show("Campo vazio. Você esqueceu de preencher o campo?", MessageType.WARN);
+        }
+        /*
+         * This is a poor date format validation, just as an example, feel free to update it
+         * as you need to.
+         */
+        else if(!widget.getValue().matches("[0-1][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9]"))
+        {
+        	this.changeState("warn");
+        	FlatMessageBox.show("Você computou um valor inválido, o formato deve ser \"dd/mm/aaaa\".", MessageType.WARN);
+        }
+        else
+        {
+			try{
+				Date date = (Date) widget.getUnformattedValue();
+				this.changeState(null);
+				FlatMessageBox.show("A data que você informou: " + date, MessageType.INFO);
+			}catch(Exception e){
+				this.changeState("warn");
+				FlatMessageBox.show("A data que você informou é inválida, tente novamente. ", MessageType.WARN);
+			}
+        	
+        }
+	}
+
+	private void changeState(String type)
+	{
+		if(type == null)
+		{
+			type = "def";
 		}
+		
+		MaskedTextBox widget = this.getDateInput();
+		Widget button = this.getButton();
+		
+		widget.removeStyleName("warn");
+		button.removeStyleName("warn");
+		
+		widget.setStyleName(type, true);
+		button.setStyleName(type, true);
 	}
 }
