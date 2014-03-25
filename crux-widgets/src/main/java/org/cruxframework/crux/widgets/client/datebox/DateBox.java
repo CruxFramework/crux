58f02733a88dbc28f170a27bcd42ac00846f0856
samuel@cruxframework.org <samuel@cruxframework.org>
2014-03-25 22:09:00 +0000

    ENHANCEMENT Issue 339: Calendar Widget

---
+++ crux-widgets/src/main/java/org/cruxframework/crux/widgets/client/datebox/DateBox.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2014 cruxframework.org.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.cruxframework.crux.widgets.client.datebox;
+
+import java.util.Date;
+
+import org.cruxframework.crux.widgets.client.datebox.gwtoverride.CruxFormat;
+import org.cruxframework.crux.widgets.client.datepicker.DatePicker;
+
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.TextBox;
+
+/**
+ * DatePicker
+ * @author Samuel Almeida Cardoso
+ */
+public class DateBox extends Composite implements IDateBox
+{
+	public static abstract class CommonDateBox extends Composite implements IDateBox
+	{
+		@Override
+		public String getBaseStyleName() 
+		{
+			return "crux-DateBox";
+		}		
+	}
+	
+	private IDateBox impl;
+	
+	public DateBox()
+	{
+		impl = GWT.create(CommonDateBox.class);
+		initWidget(impl.asWidget());
+		setStyleName(getBaseStyleName());
+	}
+
+	@Override
+	public String getBaseStyleName() 
+	{
+		return impl.getBaseStyleName();
+	}
+
+	@Override
+	public CruxFormat getFormat() 
+	{
+		return impl.getFormat();
+	}
+
+	@Override
+	public int getTabIndex() 
+	{
+		return impl.getTabIndex();
+	}
+
+	@Override
+	public TextBox getTextBox() 
+	{
+		return impl.getTextBox();
+	}
+
+	@Override
+	public DatePicker getDatePicker() 
+	{
+		return impl.getDatePicker();
+	}
+
+	@Override
+	public Date getValue() 
+	{
+		return impl.getValue();
+	}
+
+	@Override
+	public void hideDatePicker() 
+	{
+		impl.hideDatePicker();	
+	}
+
+	@Override
+	public boolean isDatePickerShowing() 
+	{
+		return impl.isDatePickerShowing();
+	}
+
+	@Override
+	public boolean isEnabled() 
+	{
+		return impl.isEnabled();
+	}
+
+	@Override
+	public void setAccessKey(char key) 
+	{
+		impl.setAccessKey(key);	
+	}
+
+	@Override
+	public void setEnabled(boolean enabled) 
+	{
+		impl.setEnabled(enabled);		
+	}
+
+	@Override
+	public void setFocus(boolean focused) 
+	{
+		impl.setFocus(focused);	
+	}
+
+	@Override
+	public void setFormat(CruxFormat format) 
+	{
+		impl.setFormat(format);
+	}
+
+	@Override
+	public void setValue(Date date) 
+	{
+		impl.setValue(date);	
+	}
+
+	@Override
+	public void showDatePicker() 
+	{
+		impl.showDatePicker();	
+	}
+}
---
+++ crux-widgets/src/main/java/org/cruxframework/crux/widgets/client/datebox/IDateBox.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2014 cruxframework.org.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.cruxframework.crux.widgets.client.datebox;
+
+import java.util.Date;
+
+import org.cruxframework.crux.widgets.client.datebox.gwtoverride.CruxFormat;
+import org.cruxframework.crux.widgets.client.datepicker.DatePicker;
+import org.cruxframework.crux.widgets.client.util.type.CruxWidget;
+
+import com.google.gwt.user.client.ui.TextBox;
+
+/**
+ * @author samuel.cardoso
+ *
+ */
+public interface IDateBox extends CruxWidget 
+{
+	public CruxFormat getFormat();
+	public int getTabIndex();
+	public TextBox getTextBox();
+	public DatePicker getDatePicker();
+	public Date getValue();
+	public void hideDatePicker();
+	public boolean isDatePickerShowing();
+	public boolean isEnabled();
+	public void setAccessKey(char key);
+	public void setEnabled(boolean enabled);
+	public void setFocus(boolean focused);
+	public void setFormat(CruxFormat format);
+	public void setValue(Date date);
+	public void showDatePicker();
+}
