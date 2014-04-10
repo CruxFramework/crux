3676eeb058a578055f875b6b860b3c63a1469680
thiago@cruxframework.org <thiago@cruxframework.org>
2014-04-10 15:02:37 +0000

    movinf super-error-handler to a  new plugin

--- src/main/java/org/cruxframework/crux/core/client/errors/SuperErrorHandlerResource.java
+++
@@ -1,9 +0,0 @@
-package org.cruxframework.crux.core.client.errors;
-
-import com.google.gwt.resources.client.ClientBundle;
-
-public interface SuperErrorHandlerResource extends ClientBundle
-{
-	@Source("supererrorhandler.css")
-	SuperErrorHandlerCSSResource css();
-}
