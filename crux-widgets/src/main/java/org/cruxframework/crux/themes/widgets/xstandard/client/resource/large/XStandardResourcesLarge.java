7738f1f484e257ab5bb917647c973f477825a427
claudio.junior@cruxframework.org <claudio.junior@cruxframework.org>
2014-06-03 14:47:43 +0000

    BUGFIX - Issues #381 #382 #383 #384

---
+++ crux-widgets/src/main/java/org/cruxframework/crux/themes/widgets/xstandard/client/resource/large/XStandardResourcesLarge.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2013 cruxframework.org.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.cruxframework.crux.themes.widgets.xstandard.client.resource.large;
+
+import org.cruxframework.crux.core.client.resources.Resource;
+import org.cruxframework.crux.core.client.screen.DeviceAdaptive.Device;
+import org.cruxframework.crux.themes.widgets.xstandard.client.resource.common.XStandardResourcesCommon;
+
+import com.google.gwt.resources.client.DataResource;
+
+/**
+ * @author Gesse Dafe
+ */
+@Resource(value="xStandardResources", supportedDevices={Device.largeDisplayArrows, Device.largeDisplayMouse, Device.largeDisplayTouch})
+public interface XStandardResourcesLarge extends XStandardResourcesCommon
+{
+	@Source({"../common/cssXStandardCommon.css","cssXStandardLarge.css"})
+	CssXStandardLarge css();
+	
+	@Source("svg-icon-arrow.svg")
+	DataResource svgIconArrow();
+	
+	@Source("svg-icon-play.svg")
+	DataResource svgIconPlay();
+	
+	@Source("svg-icon-pause.svg")
+	DataResource svgIconPause();
+}
