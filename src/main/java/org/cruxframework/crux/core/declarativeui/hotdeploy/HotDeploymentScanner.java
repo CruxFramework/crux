aa317a68542aecd84092d5dc38ffd5be3e73e108
samuel@cruxframework.org <samuel@cruxframework.org>
2014-07-16 22:22:26 +0000

    ENHANCEMENT  Issue 424: 	Improve SuperDevMode usage inside Crux

--- src/main/java/org/cruxframework/crux/core/declarativeui/hotdeploy/HotDeploymentScanner.java
+++ src/main/java/org/cruxframework/crux/core/declarativeui/hotdeploy/HotDeploymentScanner.java
@@ -16,6 +16,7 @@
 package org.cruxframework.crux.core.declarativeui.hotdeploy;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -30,11 +31,13 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.cruxframework.crux.core.client.utils.StringUtils;
+import org.cruxframework.crux.core.config.ConfigurationFactory;
 import org.cruxframework.crux.core.declarativeui.template.Templates;
 import org.cruxframework.crux.core.rebind.screen.ScreenFactory;
 import org.cruxframework.crux.core.rebind.screen.ViewFactory;
+import org.cruxframework.crux.core.server.rest.spi.HttpUtil;
 import org.cruxframework.crux.scanner.ClasspathUrlFinder;
-
 
 /**
  * @author Thiago da Rosa de Bustamante
@@ -69,7 +72,7 @@ public class HotDeploymentScanner
 	/**
 	 * 
 	 */
-	public void startScanner()
+	public void startScanner(final boolean scanFilesToCodeServer)
 	{
 		if (files.size() > 0)
 		{
@@ -81,7 +84,16 @@ public class HotDeploymentScanner
 					{
 						try
 						{
-							scan(file);
+							if(scanFilesToCodeServer)
+							{
+								if(scanFilesToCodeServer(file))
+								{
+									break;
+								}
+							} else
+							{
+								scan(file);
+							}
 						}
 						catch (Exception e) 
 						{
@@ -91,6 +103,98 @@ public class HotDeploymentScanner
 				}
 			}, 0, 5, TimeUnit.SECONDS);
 		}
+	}
+
+	/**
+	 * 
+	 * @param file
+	 */
+	protected boolean scanFilesToCodeServer(File file)
+	{
+		if (!Templates.isStarting())
+		{
+			if (file.isDirectory())
+			{
+				for (File child : file.listFiles())
+				{
+					if(scanFilesToCodeServer(child))
+					{
+						return true;
+					}
+				}
+			}
+			else
+			{
+				String fileName = file.getName();
+				boolean fileChanged = false;
+				if (fileName.endsWith("template.xml"))
+				{
+					fileChanged = checkTemplateFile(file, fileName);
+				}
+				else if (fileName.endsWith("view.xml") || fileName.endsWith("crux.xml"))
+				{
+					fileChanged = checkViewFile(file, fileName);
+				}
+				else if (fileName.endsWith("java") || fileName.endsWith("class"))
+				{
+					fileChanged = checkJavaFile(file, fileName);
+				}
+				
+				if(fileChanged)
+				{
+//					lastModified.clear(); 
+			    	//TODO: Remove when we migrate to GWT 2.6.1
+			    	//CruxBridge.removeOldGWTCodeServerCompilationFiles();
+			    	try 
+			    	{
+						recompileCodeServer();
+						return true;
+					} catch (IOException e) 
+					{
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * @param file
+	 * @param fileName
+	 */
+	private boolean checkJavaFile(File file, String fileName) 
+	{
+		long modified = file.lastModified();
+	    Long viewLastModified = lastModified.get("java_"+fileName);
+	    if (viewLastModified == null)
+	    {
+	    	lastModified.put("java_"+fileName, modified);
+	    }
+	    else if (viewLastModified < modified)
+	    {
+	    	lastModified.put("java_"+fileName, modified);
+	    	logger.info("Java file modified: ["+fileName+"].");
+	    	ScreenFactory.getInstance().clearScreenCache();
+	    	ViewFactory.getInstance().clearViewCache();
+	    	return true;
+	    }	
+	    return false;
+	}
+
+	/**
+	 * @throws IOException
+	 */
+	private void recompileCodeServer() throws IOException 
+	{
+		//curl 'http://smbh-020452:9876/recompile/crossdeviceshowcase?user.agent=gecko1_8&_callback=__gwt_bookmarklet_globals.callbacks.c0' -H 'Host: smbh-020452:9876' -H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0' -H 'Accept: */*' -H 'Accept-Language: pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3' -H 'Accept-Encoding: gzip, deflate' -H 'Referer: http://localhost:8080/crossdeviceshowcase/' -H 'Connection: keep-alive'
+		HttpUtil.wGet(
+				ConfigurationFactory.getConfigurations().superDevModeHost() +
+				":" + ConfigurationFactory.getConfigurations().superDevModePort() +
+				"/recompile/" + ConfigurationFactory.getConfigurations().superDevModeModuleToCompile(), 
+				StringUtils.isEmpty(ConfigurationFactory.getConfigurations().superDevModeBrowserUsed()) ? "" : ("user.agent=" + ConfigurationFactory.getConfigurations().superDevModeBrowserUsed()), 
+				"GET", 
+				ConfigurationFactory.getConfigurations().superDevModeLocale());
 	}
 
 	/**
@@ -128,7 +232,7 @@ public class HotDeploymentScanner
 	 * @param file
 	 * @param fileName
 	 */
-	protected void checkViewFile(File file, String fileName)
+	protected boolean checkViewFile(File file, String fileName)
     {
 	    long modified = file.lastModified();
 	    Long viewLastModified = lastModified.get("view_"+fileName);
@@ -142,7 +246,9 @@ public class HotDeploymentScanner
 	    	logger.info("View file modified: ["+fileName+"].");
 	    	ScreenFactory.getInstance().clearScreenCache();
 	    	ViewFactory.getInstance().clearViewCache();
+	    	return true;
 	    }
+	    return false;
     }
 
 	/**
@@ -150,7 +256,7 @@ public class HotDeploymentScanner
 	 * @param file
 	 * @param fileName
 	 */
-	protected void checkTemplateFile(File file, String fileName)
+	protected boolean checkTemplateFile(File file, String fileName)
     {
 	    long modified = file.lastModified();
 	    Long templateLastModified = lastModified.get("template_"+fileName);
@@ -164,13 +270,75 @@ public class HotDeploymentScanner
 	    	logger.info("Template file modified: ["+fileName+"].");
 	    	Templates.restart();
 	    	ScreenFactory.getInstance().clearScreenCache();
+	    	return true;
 	    }
+	    return false;
     }
 	
 	/**
 	 * 
 	 */
 	public static void scanWebDirs()
+	{
+		List<URL> srcDir = getNonJarFiles();
+		
+		HotDeploymentScanner scanner = new HotDeploymentScanner(srcDir.size());
+		
+		for (URL url : srcDir)
+		{
+			try
+			{
+				final File file = new File(url.toURI());
+				
+				if (isCruxXMLFile(file))
+				{
+					scanner.addFile(file);
+				}
+			}
+			catch (URISyntaxException e)
+			{
+				logger.info("Error scanning dir: ["+url.toString()+"].", e);
+			}
+		}
+		scanner.startScanner(false);
+	}
+
+	private static boolean isCruxXMLFile(final File file) 
+	{
+		return file.isDirectory() || 
+				file.getName().endsWith("template.xml") || 
+				file.getName().endsWith("view.xml") || 
+				file.getName().endsWith("crux.xml") || 
+				file.getName().endsWith("xdevice.xml");
+	}
+
+	public static void scanFilesToCodeServer() 
+	{
+		List<URL> srcDir = getNonJarFiles();
+		
+		HotDeploymentScanner scanner = new HotDeploymentScanner(srcDir.size());
+		
+		for (URL url : srcDir)
+		{
+			try
+			{
+				final File file = new File(url.toURI());
+				
+				if (isCruxXMLFile(file) ||
+						file.getName().endsWith("java") || file.getName().endsWith("class"))
+				{
+					scanner.addFile(file);
+				}
+			}
+			catch (URISyntaxException e)
+			{
+				logger.info("Error scanning dir: ["+url.toString()+"].", e);
+			}
+		}
+		scanner.startScanner(true);
+	}
+
+	private static List<URL> getNonJarFiles() 
 	{
 		URL[] dirs = ClasspathUrlFinder.findClassPaths();
 		List<URL> srcDir = new ArrayList<URL>(0);
@@ -183,29 +351,6 @@ public class HotDeploymentScanner
 				srcDir.add(url);
 			}
 		}
-		
-		HotDeploymentScanner scanner = new HotDeploymentScanner(srcDir.size());
-		
-		for (URL url : srcDir)
-		{
-			try
-			{
-				final File file = new File(url.toURI());
-				
-				if (file.isDirectory() || 
-						file.getName().endsWith("template.xml") || 
-						file.getName().endsWith("view.xml") || 
-						file.getName().endsWith("crux.xml") || 
-						file.getName().endsWith("xdevice.xml"))
-				{
-					scanner.addFile(file);
-				}
-			}
-			catch (URISyntaxException e)
-			{
-				logger.info("Error scanning dir: ["+url.toString()+"].", e);
-			}
-		}
-		scanner.startScanner();
+		return srcDir;
 	}
 }
