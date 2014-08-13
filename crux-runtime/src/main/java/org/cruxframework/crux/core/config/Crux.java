3f32ba7d4572998448375502e840f23c0bb035d4
thiago@cruxframework.org <thiago@cruxframework.org>
2014-08-13 22:24:55 +0000

    ENHANCEMENT - Issue #462

--- crux-runtime/src/main/java/org/cruxframework/crux/core/config/Crux.java
+++ crux-runtime/src/main/java/org/cruxframework/crux/core/config/Crux.java
@@ -113,22 +113,4 @@ public interface Crux
 	@DefaultServerMessage("false")
 	String disableRefreshByDefault();
 	void setDisableRefreshByDefault(String value);
-
-	@DefaultServerMessage("false")
-	String isSuperDevModeOn();
-	
-	@DefaultServerMessage("9876")
-	String superDevModePort();
-	
-	@DefaultServerMessage("localhost")
-	String superDevModeHost();
-	
-	@DefaultServerMessage("")
-	String superDevModeModuleToCompile();
-	
-	@DefaultServerMessage("")
-	String superDevModeBrowserUsed();
-
-	@DefaultServerMessage("en-US")
-	String superDevModeLocale();
 }
