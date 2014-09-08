2760a9da92dae5493a08587594bf08746ac2df33
alexandre@triggolabs.com <alexandre@triggolabs.com>
2014-09-08 20:11:36 +0000

    IGNORE - removing old projects

--- old/CruxSite/src/main/java/org/cruxframework/cruxsite/client/accessor/IndexAccessor.java
+++
@@ -1,9 +0,0 @@
-package org.cruxframework.cruxsite.client.accessor;
-
-import org.cruxframework.crux.core.client.screen.views.WidgetAccessor;
-import org.cruxframework.crux.widgets.client.rss.RssPanel;
-
-public interface IndexAccessor extends WidgetAccessor {
-	RssPanel lastBlogEntries();
-
-}
