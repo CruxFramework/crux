1d3ce40f7d5a61bcb27651d3492fd3b3e7b78451
samuel@cruxframework.org <samuel@cruxframework.org>
2014-09-16 21:06:11 +0000

    BUGFIX Issue #524: Trouble with internacionalization using placeHolder

--- src/main/java/org/cruxframework/crux/gwt/rebind/TextBoxBaseFactory.java
+++ src/main/java/org/cruxframework/crux/gwt/rebind/TextBoxBaseFactory.java
@@ -27,7 +27,7 @@ import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagAttribute
 @TagAttributes({
 	@TagAttribute("value"),
 	@TagAttribute(value="textAlignment", type=TextBoxBaseFactory.TextAlign.class, processor=TextBoxBaseFactory.TextAlignmentProcessor.class),
-	@TagAttribute(value="placeHolder", type=String.class, processor=TextBoxBaseFactory.PlaceHolderProcessor.class)
+	@TagAttribute(value="placeHolder", type=String.class, processor=TextBoxBaseFactory.PlaceHolderProcessor.class, supportsI18N=true)
 })
 public abstract class TextBoxBaseFactory extends ValueBoxBaseFactory
 {	
