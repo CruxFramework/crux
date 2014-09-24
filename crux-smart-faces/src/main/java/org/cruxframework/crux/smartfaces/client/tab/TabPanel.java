a43d8d18dea75b2cfd8501d8d6dd464c0c8adc37
bruno@triggolabs.com <bruno@triggolabs.com>
2014-09-24 13:50:23 +0000

    IGNORE - correcting codeStyle

--- crux-smart-faces/src/main/java/org/cruxframework/crux/smartfaces/client/tab/TabPanel.java
+++ crux-smart-faces/src/main/java/org/cruxframework/crux/smartfaces/client/tab/TabPanel.java
@@ -69,7 +69,6 @@ public class TabPanel extends Composite implements HasAnimation, HasBeforeSelect
 			public void onSelection(SelectionEvent<Integer> event)
 			{
 				showTabContent(event.getSelectedItem());
-				
 			}
 		});
 		
