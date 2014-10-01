e22b7bc86153650086daa68e0cebebbcf6fcc92b
thiago@cruxframework.org <thiago@cruxframework.org>
2014-10-01 17:21:33 +0000

    BUGFIX - Issue #548 - Missing DialogAnimation attribute o HasDialogAnimationFactory

--- crux-smart-faces/src/main/java/org/cruxframework/crux/smartfaces/rebind/dialog/HasDialogAnimationFactory.java
+++ crux-smart-faces/src/main/java/org/cruxframework/crux/smartfaces/rebind/dialog/HasDialogAnimationFactory.java
@@ -15,14 +15,40 @@
  */
 package org.cruxframework.crux.smartfaces.rebind.dialog;
 
+import org.cruxframework.crux.core.rebind.AbstractProxyCreator.SourcePrinter;
+import org.cruxframework.crux.core.rebind.screen.widget.AttributeProcessor;
+import org.cruxframework.crux.core.rebind.screen.widget.WidgetCreator;
 import org.cruxframework.crux.core.rebind.screen.widget.WidgetCreatorContext;
 import org.cruxframework.crux.core.rebind.screen.widget.creator.HasAnimationFactory;
+import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagAttribute;
+import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagAttributes;
+import org.cruxframework.crux.smartfaces.client.dialog.animation.DialogAnimation;
 
 /**
  * A helper class to help on HasDialogAnimation widgets creation, based on crux pages metadata.
  * @author Thiago da Rosa de Bustamante
  *
  */
+@TagAttributes({
+	@TagAttribute(value="animation", processor=HasDialogAnimationFactory.AnimationProcessor.class, type=HasDialogAnimationFactory.DialogAnimations.class)
+})
 public interface HasDialogAnimationFactory<C extends WidgetCreatorContext> extends HasAnimationFactory<C>
 {
+	public static enum DialogAnimations{bounce, bounceUpDown, bounceLeft, bounceRight, bounceDownUp, fade, fadeDownUp, 
+		fadeUpDown, fadeLeft, fadeRight, fadeDownUpBig, fadeUpDownBig, fadeLeftBig, fadeRightBig, flipX, flipY, lightSpeed, 
+		rotate, rotateDownLeft, rotateDownRight, rotateUpLeft, rotateUpRight, slideDown, slideLeft, slideRight, roll}
+	
+	public static class AnimationProcessor extends AttributeProcessor<WidgetCreatorContext>
+    {
+		public AnimationProcessor(WidgetCreator<?> widgetCreator)
+        {
+	        super(widgetCreator);
+        }
+
+		@Override
+        public void processAttribute(SourcePrinter out, WidgetCreatorContext context, String attributeValue)
+        {
+	        out.println(context.getWidget()+".setAnimation("+DialogAnimation.class.getCanonicalName()+"."+attributeValue+");");
+        }
+    }
 }
