3d6542a699ef31f8087dcb262c225c4bd867acd9
bruno@triggolabs.com <bruno@triggolabs.com>
2014-10-03 16:49:04 +0000

    Issue #552 - ENHANCEMENT - : Add in crux-dev SwapEvent

---
+++ crux-dev/src/main/java/org/cruxframework/crux/core/client/event/swap/SwapEvent.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2014 cruxframework.org.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.cruxframework.crux.core.client.event.swap;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+/**
+ * 
+ * @author Thiago da Rosa de Bustamante
+ *
+ */
+public class SwapEvent extends GwtEvent<SwapHandler>
+{
+	private static Type<SwapHandler> TYPE = new Type<SwapHandler>();
+
+	/**
+	 * 
+	 */
+	public SwapEvent()
+	{
+	}
+
+	/**
+	 * @return
+	 */
+	public static Type<SwapHandler> getType()
+	{
+		return TYPE;
+	}
+
+	@Override
+	protected void dispatch(SwapHandler handler)
+	{
+		handler.onSwap(this);
+	}
+
+	@Override
+	public Type<SwapHandler> getAssociatedType()
+	{
+		return TYPE;
+	}
+
+	public static <T> void fire(HasSwapHandlers source) 
+	{
+		if (TYPE != null) 
+		{
+			SwapEvent event = new SwapEvent();
+			source.fireEvent(event);
+		}
+	}	
+}
