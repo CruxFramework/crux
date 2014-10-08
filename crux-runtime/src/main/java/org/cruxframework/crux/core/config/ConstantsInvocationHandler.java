8b4756f9110986d95c5890f82db98686b74f8206
samuel@cruxframework.org <samuel@cruxframework.org>
2014-10-08 16:50:34 +0000

    BUGFIX Issue #553

--- crux-runtime/src/main/java/org/cruxframework/crux/core/config/ConstantsInvocationHandler.java
+++ crux-runtime/src/main/java/org/cruxframework/crux/core/config/ConstantsInvocationHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 cruxframework.org.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.cruxframework.crux.core.config;
 
 import java.io.IOException;
@@ -195,22 +210,29 @@ public abstract class ConstantsInvocationHandler implements InvocationHandler
 		PropertyResourceBundle properties = null;
 		try
 		{
-			properties = (PropertyResourceBundle) PropertyResourceBundle.getBundle(targetInterface.getCanonicalName(), locale);
+			properties = (PropertyResourceBundle) PropertyResourceBundle.getBundle(targetInterface.getSimpleName(), locale);
 		}
-		catch (Throwable e) 
+		catch (Exception e) 
 		{
-			try 
+			try
 			{
-				String resourceName = "/"+targetInterface.getName().replaceAll("\\.", "/") + ".properties";
-				InputStream input = targetInterface.getClassLoader().getResourceAsStream(resourceName);
-				if (input != null)
+				properties = (PropertyResourceBundle) PropertyResourceBundle.getBundle(targetInterface.getCanonicalName(), locale);
+			}
+			catch (Exception f) 
+			{
+				try 
 				{
-					properties = new PropertyResourceBundle(input);
-				}
-			} 
-			catch (IOException e1) 
-			{
-				throw new MessageException(e.getMessage(), e);
+					String resourceName = "/"+targetInterface.getName().replaceAll("\\.", "/") + ".properties";
+					InputStream input = targetInterface.getClassLoader().getResourceAsStream(resourceName);
+					if (input != null)
+					{
+						properties = new PropertyResourceBundle(input);
+					}
+				} 
+				catch (IOException e1) 
+				{
+					throw new MessageException(e.getMessage(), e);
+				}				
 			}
 		}
 		return properties;
