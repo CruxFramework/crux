018e3f5c302b73be4411e3f06a52fa9f45785499
samuel@cruxframework.org <samuel@cruxframework.org>
2014-10-29 17:20:17 +0000

    BUGFIX Issue #565: Migrate Blur and Focus events to Crux-dev

--- src/main/java/org/cruxframework/crux/core/rebind/event/BeforeBlurEvtBind.java
+++ src/main/java/org/cruxframework/crux/core/rebind/event/BeforeBlurEvtBind.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.cruxframework.crux.core.client.event;
+package org.cruxframework.crux.core.rebind.event;
 
 import org.cruxframework.crux.core.client.event.focusblur.BeforeBlurEvent;
 import org.cruxframework.crux.core.client.event.focusblur.BeforeBlurHandler;
