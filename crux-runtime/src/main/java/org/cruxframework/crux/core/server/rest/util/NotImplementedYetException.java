542d0c5bfa8c16399e488486402bbac65a6d9fcc
samuel@cruxframework.org <samuel@cruxframework.org>
2014-11-18 21:42:17 +0000

    IGNORE adding tag 5.2.1

--- crux-runtime/src/main/java/org/cruxframework/crux/core/server/rest/util/NotImplementedYetException.java
+++
@@ -1,6 +0,0 @@
-package org.cruxframework.crux.core.server.rest.util;
-
-public class NotImplementedYetException extends RuntimeException
-{
-    private static final long serialVersionUID = 1049055181630929649L;
-}
