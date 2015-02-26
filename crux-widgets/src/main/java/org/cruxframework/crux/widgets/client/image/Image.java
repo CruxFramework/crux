b779d2f2e2e0ca676fb37248a85d7ad1b733d228
samuel@cruxframework.org <samuel@cruxframework.org>
2015-02-26 18:19:57 +0000

    IGNORE merging with branch 5.3 revision 5935

--- crux-widgets/src/main/java/org/cruxframework/crux/widgets/client/image/Image.java
+++ crux-widgets/src/main/java/org/cruxframework/crux/widgets/client/image/Image.java
@@ -15,6 +15,7 @@
  */
 package org.cruxframework.crux.widgets.client.image;
 
+import org.cruxframework.crux.core.client.image.GWTFixImage;
 import org.cruxframework.crux.widgets.client.event.HasSelectHandlers;
 import org.cruxframework.crux.widgets.client.event.SelectEvent;
 import org.cruxframework.crux.widgets.client.event.SelectHandler;
@@ -308,9 +309,11 @@ public class Image extends Composite implements HasSelectHandlers, HasLoadHandle
 	
 	public void setUrlAndVisibleRect(final SafeUri url, final int left, final int top, final int width, final int height)
 	{
-		new GWTFixImage() {
+		new GWTFixImage(impl) 
+		{
 			@Override
-			public void callHowToImplementInnerSetVisibleRect() {
+			public void callHowToImplementInnerSetVisibleRect() 
+			{
 				impl.setUrlAndVisibleRect(url, left, top, width, height);		
 			}
 		};
@@ -318,9 +321,11 @@ public class Image extends Composite implements HasSelectHandlers, HasLoadHandle
 
 	public void setUrlAndVisibleRect(final String url, final int left, final int top, final int width, final int height)
 	{
-		new GWTFixImage() {
+		new GWTFixImage(impl) 
+		{
 			@Override
-			public void callHowToImplementInnerSetVisibleRect() {
+			public void callHowToImplementInnerSetVisibleRect() 
+			{
 				impl.setUrlAndVisibleRect(url, left, top, width, height);		
 			}
 		};
@@ -328,48 +333,15 @@ public class Image extends Composite implements HasSelectHandlers, HasLoadHandle
 
 	public void setVisibleRect(final int left, final int top, final int width, final int height)
 	{
-		new GWTFixImage() {
+		new GWTFixImage(impl) {
 			@Override
-			public void callHowToImplementInnerSetVisibleRect() {
+			public void callHowToImplementInnerSetVisibleRect() 
+			{
 				impl.setVisibleRect(left, top, width, height);		
 			}
 		};
 	}
 
-	/*
-	   note1: Issue submitted to GWT:
-	   Issue 8325: 	GWT Image: using 'VisibleRect' method erases previous stylesheets declared
-	   
-	   Until version 2.5.1 GWT doesn't copy any old 
-	   properties to the new element created inside a Clipped 
-	   or NotClipped constructor. So they are lost at some point.
-	   
-	   In order to copy the properties I had to use something like: 
-	   impl.setProperty(...);
-	   for each one of them because the following method 
-	   (intended to copy all properties) doesn't work here:
-	   getElement().setAttribute("style", currentStyle + oldStyle);
-	 */
-	private abstract class GWTFixImage
-	{
-		public GWTFixImage() {
-			setVisibleRect();
-		}
-		
-		public abstract void callHowToImplementInnerSetVisibleRect();
-		
-		public void setVisibleRect()
-		{
-			boolean oldStyleHasDisplayNone = impl.getElement().getAttribute("style").contains("display: none");
-			String title = impl.getElement().getTitle();
-			String styleName = impl.getStyleName();
-			callHowToImplementInnerSetVisibleRect();
-			impl.setVisible(!oldStyleHasDisplayNone);
-			impl.setTitle(title);
-			impl.setStyleName(styleName);
-		}
-	}
-	
 	@Override
 	public boolean isEnabled()
 	{
