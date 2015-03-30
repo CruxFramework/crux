0f70b14ffced7f7317119ee9e7f085e51230b201
samuel@cruxframework.org <samuel@cruxframework.org>
2015-03-30 14:39:14 +0000

    IGNORE fixing SVN crazyness: it's incorrectly telling that this package has a modification

---
+++ crux-dev/src/main/java/org/cruxframework/crux/core/client/event/paste/HasPasteHandlers.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2011 cruxframework.org.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.cruxframework.crux.core.client.event.paste;
+
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.event.shared.HasHandlers;
+
+/**
+ * Source for PasteEvents. All classes that implements this interface must explicitly invoke 
+ * <code>org.cruxframework.crux.widgets.client.event.paste.PasteRegisterFactory.getPasteRegister().addNativeHandlerForPaste(this, this.getElement());</code> 
+ * once in its constructor. 
+ * @author Gesse S. F. Dafe
+ */
+public interface HasPasteHandlers extends HasHandlers 
+{
+	HandlerRegistration addPasteHandler(PasteHandler handler);
+}
