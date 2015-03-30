ac5175d36a9977da2496a4d4794766e7d0deaf05
samuel@cruxframework.org <samuel@cruxframework.org>
2015-03-30 15:55:33 +0000

    IGNORE merge with branch 1.2 revision 6052

--- crux-smart-faces/src/main/java/org/cruxframework/crux/smartfaces/rebind/disposal/menudisposal/SideMenuDisposalFactory.java
+++ crux-smart-faces/src/main/java/org/cruxframework/crux/smartfaces/rebind/disposal/menudisposal/SideMenuDisposalFactory.java
@@ -48,10 +48,14 @@ import org.cruxframework.crux.smartfaces.rebind.Constants;
 import org.cruxframework.crux.smartfaces.rebind.disposal.menudisposal.SideMenuDisposalFactory.DisposalLayoutContext;
 
 
-@DeclarativeFactory(library=Constants.LIBRARY_NAME,id="sideMenuDisposal",targetWidget=SideMenuDisposal.class,description="A component to define the page's layout. It contains a header, a interactive menu, a content panel and a footer.")
+@DeclarativeFactory(library=Constants.LIBRARY_NAME,id="sideMenuDisposal", 
+					targetWidget=SideMenuDisposal.class, 
+					description="A component to define the page's layout. It contains a header, a interactive menu, a content panel and a footer.")
 @TagAttributes({
-	@TagAttribute(value="menuPositioning", type=MenuPosition.class, defaultValue="LEFT"),
-	@TagAttribute(value="historyControlPrefix",defaultValue="view")
+	@TagAttribute(value="menuPositioning", type=MenuPosition.class, defaultValue="LEFT", 
+				  description="The position where the menu will be placed"),
+	@TagAttribute(value="historyControlPrefix",defaultValue="view", 
+				  description="The name of the token to be used to identify the history state. This token will be used as part of the URL to the view showed by this panel.")
 })
 @TagChildren({
 	@TagChild(SideMenuDisposalFactory.DisposalChildrenProcessor.class)
@@ -74,22 +78,32 @@ public class SideMenuDisposalFactory extends WidgetCreator<DisposalLayoutContext
 	})
 	public static class DisposalChildrenProcessor extends ChoiceChildProcessor<DisposalLayoutContext>{}
 	
-    @TagConstraints(minOccurs="1", maxOccurs="1", tagName="view")
+    @TagConstraints(minOccurs="1", maxOccurs="1", tagName="view", 
+    				description="A view to be rendered into this view container.")
     @TagAttributesDeclaration({
-    	@TagAttributeDeclaration(value="name", required=true)
+		@TagAttributeDeclaration(value="id", description="The view identifier."),
+    	@TagAttributeDeclaration(value="name", required=true, description="The name of the view.")
     })
     public static class ViewProcessor extends WidgetChildProcessor<DisposalLayoutContext>
     {
     	@Override
     	public void processChildren(SourcePrinter out, DisposalLayoutContext context) throws CruxGeneratorException
     	{
-    		out.println("if(!"+context.getWidget()+".getCurrentHistoryItem().contains(\"!\"))");
-    		out.println(context.getWidget()+".showView("+EscapeUtils.quote(context.readChildProperty("name"))+");");
+    		String viewId = context.readChildProperty("id");
+    		String viewName = context.readChildProperty("name");
+    		
+    		if (StringUtils.isEmpty(viewId))
+    		{
+    			viewId = viewName;
+    		}
+    		out.println("if(!"+context.getWidget()+".isHistoryTarget())");
+    		out.println(context.getWidget()+".showView("+EscapeUtils.quote(viewName)+", "+EscapeUtils.quote(viewId)+");");
     	}
     }
 	
 	
-	@TagConstraints(minOccurs="1",maxOccurs="1", tagName="smallHeader")
+	@TagConstraints(minOccurs="1",maxOccurs="1", tagName="smallHeader", 
+					description="The header section to be used on small devices.")
 	@TagChildren({
 		@TagChild(value=SideMenuDisposalFactory.SmallHeaderProcessor.class)
 	})
@@ -97,7 +111,8 @@ public class SideMenuDisposalFactory extends WidgetCreator<DisposalLayoutContext
 	{
 	}
 	
-	@TagConstraints(minOccurs="1",maxOccurs="1", tagName="largeHeader")
+	@TagConstraints(minOccurs="1",maxOccurs="1", tagName="largeHeader", 
+					description="The header section to be used on large devices.")
 	@TagChildren({
 		@TagChild(value=SideMenuDisposalFactory.LargeHeaderProcessor.class)
 	})
@@ -110,9 +125,11 @@ public class SideMenuDisposalFactory extends WidgetCreator<DisposalLayoutContext
 		VERTICAL_TREE, VERTICAL_SLIDE, VERTICAL_ACCORDION, VERTICAL_DROPDOWN;
 	}
 
-	@TagConstraints(maxOccurs="1",minOccurs="1",tagName="mainMenu")
+	@TagConstraints(maxOccurs="1",minOccurs="1",tagName="mainMenu", 
+					description="The menu section of this component.")
 	@TagAttributesDeclaration({
-		@TagAttributeDeclaration(value="menuType", type=SideDisposalMenuType.class, defaultValue="VERTICAL_DROPDOWN")
+		@TagAttributeDeclaration(value="menuType", type=SideDisposalMenuType.class, defaultValue="VERTICAL_DROPDOWN", 
+					description="The type of the menu to be displayed.")
 	})
 	@TagChildren({
 		@TagChild(SideMenuDisposalFactory.MenuItemProcessor.class)
@@ -143,7 +160,8 @@ public class SideMenuDisposalFactory extends WidgetCreator<DisposalLayoutContext
 		}
 	}
     
-    @TagConstraints(maxOccurs="unbounded", minOccurs="0",tagName="menuItem")
+    @TagConstraints(maxOccurs="unbounded", minOccurs="0",tagName="menuItem", 
+    				description="A menu item.")
 	@TagAttributesDeclaration({
 		@TagAttributeDeclaration(value="targetView",required=false,description="Defines the target view that will be displayed on clicking it"),
 		@TagAttributeDeclaration(value="label", required=true, description="Defines the label that will be displayed", supportsI18N=true)
@@ -196,7 +214,8 @@ public class SideMenuDisposalFactory extends WidgetCreator<DisposalLayoutContext
 		}
 	}
 	
-	@TagConstraints(minOccurs="1",maxOccurs="1", tagName="footer")
+	@TagConstraints(minOccurs="1",maxOccurs="1", tagName="footer", 
+					description="The footer section of this container.")
 	@TagChildren({
 		@TagChild(value=SideMenuDisposalFactory.FooterProcessor.class)
 	})
