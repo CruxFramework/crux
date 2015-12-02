1564185d71f38c38a9381fdca6bae7cef3c64e68
thiagobustamante <thiago@cruxframework.org>
2015-12-01 22:01:38 -0200

    Issue #898

---
+++ crux-runtime/src/main/java/org/cruxframework/crux/core/annotation/processor/CruxAnnotationProcessor.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2015 cruxframework.org.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.cruxframework.crux.core.annotation.processor;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.lang.model.SourceVersion;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
+
+/**
+ * @author Thiago da Rosa de Bustamante
+ *
+ */
+public abstract class CruxAnnotationProcessor extends AbstractProcessor
+{
+	@Override
+	public SourceVersion getSupportedSourceVersion()
+	{
+		return SourceVersion.latestSupported();
+	}
+
+	protected FileObject createResourceFile(final String name) throws IOException
+	{
+		final Filer filer = processingEnv.getFiler();
+		FileObject file = filer.createResource(StandardLocation.CLASS_OUTPUT, "", name);
+		return file;
+	}
+	
+	protected FileObject getResourceFile(final String name) throws IOException 
+	{
+		final Filer filer = processingEnv.getFiler();
+		FileObject file;
+        try
+        {
+	        file = filer.getResource(StandardLocation.CLASS_OUTPUT, "", name);
+        }
+        catch (FileNotFoundException e)
+        {
+	        return null;
+        }
+		return file;
+	}
+	
+	protected InputStream getResourceFileStream(final String name) throws IOException
+	{
+        try
+        {
+        	FileObject previousRestFile = getResourceFile(name);
+        	return previousRestFile.openInputStream();
+        }
+        catch (FileNotFoundException e)
+        {
+	        return null;
+        }
+	}
+	
+	
+	protected boolean isIncremental()
+    {
+		String incremental = processingEnv.getOptions().get("Crux.apt.incremental");
+		return Boolean.parseBoolean(incremental);
+    }
+}
